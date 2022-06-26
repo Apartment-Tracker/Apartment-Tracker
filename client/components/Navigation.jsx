79e6e4f55a465487e6a1068278a13e34f8a6736b
Matthew Yeon <83806064+myeon7@users.noreply.github.com>
2022-06-26 18:17:33 -0400

    Update Navigation.jsx
    Formatting consistency updated

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -3,21 +3,19 @@ import '../stylesheets/navigation.scss';
 import { BrowserRouter } from 'react-router-dom';
 
 
-
-
 const Navigation = props => {
   return(
-      <nav >
-        <div className='category'>
-          <div id ="logo"></div>
-          <div >
+      <nav>
+        <div className="category">
+          <div id="logo"></div>
+          <div>
             <h2>Category Name</h2>
-            <p className='back'>Back to Category</p>
+            <p className="back">Back to Category</p>
           </div>
           <div>
-            <input type = "search" id ="search" name = "search"/>
+            <input type="search" id="search" name="search"/>
           </div>
-          <div id ="profile"></div>
+          <div id="profile"></div>
         </div>
       </nav>
     
