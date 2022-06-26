aa73bc8efa5c621454dba9f74b5d16ecb5dd2e40
Kristen Lu <kristenlu@Kristens-iMac.local>
2022-06-26 14:50:46 -0700

    navigation-done

--- client/App.jsx
+++ client/App.jsx
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation.jsx';
 import Main from './components/Main.jsx';
 import './stylesheets/style.scss';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './components/Home.jsx'
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navigation />
-        <Main />
-        <ul>
-          <li>Hi</li>
-          <li>Hi</li>
-          <li>Hi</li>
-        </ul>
-      </div>
+          <div>
+            <Navigation />
+           {/* <Route path = "/home" component={Home}/> */}
+            <Main />
+          </div>
     );
   };
 };
--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import '../stylesheets/navigation.scss';
+import { BrowserRouter } from 'react-router-dom';
+
+
+
 
 const Navigation = props => {
   return(
-    <>
-    </>
+      <nav >
+        <div className='category'>
+          <div id ="logo"></div>
+          <div >
+            <h2>Category Name</h2>
+            <p className='back'>Back to Category</p>
+          </div>
+          <div>
+            <input type = "search" id ="search" name = "search"/>
+          </div>
+          <div id ="profile"></div>
+        </div>
+      </nav>
+    
   );
 };
 
